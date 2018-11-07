b6de709db622d3f0977fc827b7920fb8803c6183
tomasz.bar3@gmail.com <Niuteczka1!>
2018-11-07 19:44:04 +0100

    Revert "organizing code"
    This reverts commit c5c2c6b

---
+++ src/test/java/com/sda/tests/simpletest/SimpleTest.java
@@ -0,0 +1,12 @@
+package com.sda.tests.simpletest;
+
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+
+    @Test
+    void simpleTest(){
+
+    }
+
+}
---
+++ src/test/java/com/sda/tests/strings/StringMethodsTest3.java
@@ -0,0 +1,12 @@
+package com.sda.tests.strings;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringMethodsTest3 {
+
+    @Test
+    void isPalindrome() {
+    }
+}
